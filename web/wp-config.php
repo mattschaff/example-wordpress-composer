8dabb72dc3384c8c376a2b80047379297da7c54a
mattschaff <matthew.schaff@gmail.com>
2021-11-16 14:40:36 -0500

    Remove pantheon-specific config

--- web/wp-config.php
+++ web/wp-config.php
@@ -14,19 +14,6 @@ $rootPath = realpath( __DIR__ . '/..' );
  */
 require_once( $rootPath . '/vendor/autoload.php' );
 
-/*
- * Fetch .env
- */
-if ( ! isset( $_ENV['PANTHEON_ENVIRONMENT'] ) && file_exists( $rootPath . '/.env' ) ) {
-	$dotenv = Dotenv\Dotenv::create($rootPath);
-	$dotenv->load();
-	$dotenv->required( array(
-		'DB_NAME',
-		'DB_USER',
-		'DB_HOST',
-	) )->notEmpty();
-}
-
 /**
  * Disallow on server file edits
  */
@@ -36,138 +23,77 @@ define( 'DISALLOW_FILE_MODS', true );
 /**
  * Force SSL
  */
-define( 'FORCE_SSL_ADMIN', true );
+define( 'FORCE_SSL_ADMIN', false );
 
 /**
  * Limit post revisions
  */
 define( 'WP_POST_REVISIONS', 3 );
 
-/*
- * If NOT on Pantheon
+/**
+ * Define site and home URLs
  */
-if ( ! isset( $_ENV['PANTHEON_ENVIRONMENT'] ) ):
-	/**
-	 * Define site and home URLs
-	 */
-	// HTTP is still the default scheme for now.
-	$scheme = 'http';
-	// If we have detected that the end use is HTTPS, make sure we pass that
-	// through here, so <img> tags and the like don't generate mixed-mode
-	// content warnings.
-	if ( isset( $_SERVER['HTTP_USER_AGENT_HTTPS'] ) && $_SERVER['HTTP_USER_AGENT_HTTPS'] == 'ON' ) {
-		$scheme = 'https';
-	}
-	$site_url = getenv( 'WP_HOME' ) !== false ? getenv( 'WP_HOME' ) : $scheme . '://' . $_SERVER['HTTP_HOST'] . '/';
-	define( 'WP_HOME', $site_url );
-	define( 'WP_SITEURL', $site_url . 'wp/' );
+// HTTP is still the default scheme for now.
 
-	/**
-	 * Set Database Details
-	 */
+$scheme = 'http';
+// If we have detected that the end use is HTTPS, make sure we pass that
+// through here, so <img> tags and the like don't generate mixed-mode
+// content warnings.
+if ( isset( $_SERVER['HTTP_USER_AGENT_HTTPS'] ) && $_SERVER['HTTP_USER_AGENT_HTTPS'] == 'ON' ) {
+	$scheme = 'https';
+}
+
+$site_url = getenv( 'WP_HOME' ) !== false ? getenv( 'WP_HOME' ) : $scheme . '://' . $_SERVER['HTTP_HOST'] . '/';
+define( 'WP_HOME', $site_url );
+define( 'WP_SITEURL', $site_url . 'wp/' );
+
+/**
+ * Set Database Details
+ */
+/**
+ * Set Database Details
+ */
+// Include docksal settings.php when appropriate.
+if (isset($_SERVER['DOCKSAL_LOCAL'])) {
+	define( 'DB_NAME', getenv('MYSQL_DATABASE') );
+	define( 'DB_USER', getenv('MYSQL_USER') );
+	define( 'DB_PASSWORD', getenv('MYSQL_PASSWORD') );
+	define( 'DB_HOST', getenv('MYSQL_HOST') );
+	// Pass "https" protocol from reverse proxies
+	if (isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] == 'https') {
+		$_SERVER['HTTPS'] = 'on';
+	}
+}
+else {
 	define( 'DB_NAME', getenv( 'DB_NAME' ) );
 	define( 'DB_USER', getenv( 'DB_USER' ) );
 	define( 'DB_PASSWORD', getenv( 'DB_PASSWORD' ) !== false ? getenv( 'DB_PASSWORD' ) : '' );
 	define( 'DB_HOST', getenv( 'DB_HOST' ) );
-
-	/**
-	 * Set debug modes
-	 */
-	define( 'WP_DEBUG', getenv( 'WP_DEBUG' ) === 'true' ? true : false );
-	define( 'IS_LOCAL', getenv( 'IS_LOCAL' ) !== false ? true : false );
-
-	/**#@+
-	 * Authentication Unique Keys and Salts.
-	 *
-	 * Change these to different unique phrases!
-	 * You can generate these using the {@link https://api.wordpress.org/secret-key/1.1/salt/ WordPress.org secret-key service}
-	 * You can change these at any point in time to invalidate all existing cookies. This will force all users to have to log in again.
-	 *
-	 * @since 2.6.0
-	 */
-	define( 'AUTH_KEY',         'put your unique phrase here' );
-	define( 'SECURE_AUTH_KEY',  'put your unique phrase here' );
-	define( 'LOGGED_IN_KEY',    'put your unique phrase here' );
-	define( 'NONCE_KEY',        'put your unique phrase here' );
-	define( 'AUTH_SALT',        'put your unique phrase here' );
-	define( 'SECURE_AUTH_SALT', 'put your unique phrase here' );
-	define( 'LOGGED_IN_SALT',   'put your unique phrase here' );
-	define( 'NONCE_SALT',       'put your unique phrase here' );
-
-endif;
-
-/*
- * If on Pantheon
+}
+/**
+ * Set debug modes
  */
-if ( isset( $_ENV['PANTHEON_ENVIRONMENT'] ) ):
+define( 'WP_DEBUG', getenv( 'WP_DEBUG' ) === 'true' ? true : false );
+define( 'IS_LOCAL', getenv( 'IS_LOCAL' ) !== false ? true : false );
 
-	// ** MySQL settings - included in the Pantheon Environment ** //
-	/** The name of the database for WordPress */
-	define( 'DB_NAME', $_ENV['DB_NAME'] );
+/**#@+
+ * Authentication Unique Keys and Salts.
+ *
+ * Change these to different unique phrases!
+ * You can generate these using the {@link https://api.wordpress.org/secret-key/1.1/salt/ WordPress.org secret-key service}
+ * You can change these at any point in time to invalidate all existing cookies. This will force all users to have to log in again.
+ *
+ * @since 2.6.0
+ */
+define('AUTH_KEY',         '|@B^ygfa~8=co#9eut56E1c,h};Fp|}PAO|xBi~L%nS$8.9/)%#QDO}N(!Y-E^zF');
+define('SECURE_AUTH_KEY',  '-#_zHGwIjYA]^k~oh%+(./b-5H=xZ_]~1F0;8~42x}| <-[29ScC--CxBidE<037');
+define('LOGGED_IN_KEY',    'XUwR6)&G&~g7q)L!qZ; ]D&AMZhL8-g`lW5j(3iw@ieSjqowbs)6)6@M.v+v9te6');
+define('NONCE_KEY',        '<Y%GwtqGPP^IIDUF1%b4g|stz*1S0h;&:y!elJL:9%y8j1mmWyI1+hygr>Fc{JS-');
+define('AUTH_SALT',        'T+xaTz-Gb8o7ATOm|&1ad^|EwKk$Lgg2>-r&e4NxS2E|EoA!?!t},l57O}KYdDyI');
+define('SECURE_AUTH_SALT', '7@zB<Sk&LL|j2a; q/e8:Y<t%=kD|w[~}sIz@.wz-y=):TA|Y|$o9+?@qk>AE3iF');
+define('LOGGED_IN_SALT',   '4||jkfL|%Co/4ck1-76DJ_E~jL4nP#?R&$8qD;*Uy7fctq{WhjL%wn**sugpY>Jl');
+define('NONCE_SALT',       '$WC<aor+L=-e^iV-;-4G41!mirb1aT~2niRS+7A{gE|dX/V=E+adsH(IE/(lmn|S');
 
-	/** MySQL database username */
-	define( 'DB_USER', $_ENV['DB_USER'] );
-
-	/** MySQL database password */
-	define( 'DB_PASSWORD', $_ENV['DB_PASSWORD'] );
-
-	/** MySQL hostname; on Pantheon this includes a specific port number. */
-	define( 'DB_HOST', $_ENV['DB_HOST'] . ':' . $_ENV['DB_PORT'] );
-
-	/** Database Charset to use in creating database tables. */
-	define( 'DB_CHARSET', 'utf8' );
-
-	/** The Database Collate type. Don't change this if in doubt. */
-	define( 'DB_COLLATE', '' );
-
-	/**#@+
-	 * Authentication Unique Keys and Salts.
-	 *
-	 * Change these to different unique phrases!
-	 * You can generate these using the {@link https://api.wordpress.org/secret-key/1.1/salt/ WordPress.org secret-key service}
-	 * You can change these at any point in time to invalidate all existing cookies. This will force all users to have to log in again.
-	 *
-	 * Pantheon sets these values for you also. If you want to shuffle them you
-	 * can do so via your dashboard.
-	 *
-	 * @since 2.6.0
-	 */
-	define( 'AUTH_KEY', $_ENV['AUTH_KEY'] );
-	define( 'SECURE_AUTH_KEY', $_ENV['SECURE_AUTH_KEY'] );
-	define( 'LOGGED_IN_KEY', $_ENV['LOGGED_IN_KEY'] );
-	define( 'NONCE_KEY', $_ENV['NONCE_KEY'] );
-	define( 'AUTH_SALT', $_ENV['AUTH_SALT'] );
-	define( 'SECURE_AUTH_SALT', $_ENV['SECURE_AUTH_SALT'] );
-	define( 'LOGGED_IN_SALT', $_ENV['LOGGED_IN_SALT'] );
-	define( 'NONCE_SALT', $_ENV['NONCE_SALT'] );
-	/**#@-*/
-
-	/** A couple extra tweaks to help things run well on Pantheon. **/
-	if ( isset( $_SERVER['HTTP_HOST'] ) ) {
-		// HTTP is still the default scheme for now.
-		$scheme = 'http';
-		// If we have detected that the end use is HTTPS, make sure we pass that
-		// through here, so <img> tags and the like don't generate mixed-mode
-		// content warnings.
-		if ( isset( $_SERVER['HTTP_USER_AGENT_HTTPS'] ) && $_SERVER['HTTP_USER_AGENT_HTTPS'] == 'ON' ) {
-			$scheme = 'https';
-		}
-		define( 'WP_HOME', $scheme . '://' . $_SERVER['HTTP_HOST'] );
-		define( 'WP_SITEURL', $scheme . '://' . $_SERVER['HTTP_HOST'] . '/wp' );
-
-	}
-
-	// Force the use of a safe temp directory when in a container
-	if ( defined( 'PANTHEON_BINDING' ) ):
-		define( 'WP_TEMP_DIR', sprintf( '/srv/bindings/%s/tmp', PANTHEON_BINDING ) );
-	endif;
-
-	// FS writes aren't permitted in test or live, so we should let WordPress know to disable relevant UI
-	if ( in_array( $_ENV['PANTHEON_ENVIRONMENT'], array( 'test', 'live' ) ) && ! defined( 'DISALLOW_FILE_MODS' ) ) :
-		define( 'DISALLOW_FILE_MODS', true );
-	endif;
-
-endif;
 
 /*
 * Define wp-content directory outside of WordPress core directory
